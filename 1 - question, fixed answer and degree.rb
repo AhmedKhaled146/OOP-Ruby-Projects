1a776b0b1ae4fb076c7eb3605b6f4fcdab677398
AhmedKhaled146 <a7med74yaso@gmail.com>
2024-08-22 22:18:46 +0300

    add 2 projects

---
+++ 1 - question, fixed answer and degree.rb
@@ -0,0 +1,36 @@
+class Question
+  attr_accessor :prompt, :answer
+
+  def initialize(prompt, answer)
+    @prompt = prompt
+    @answer = answer
+  end
+end
+
+q1 = "We define an algorithm’s worst-case time complexity by using?\n(a) Big-O Notation\n(b) Omega Notation\n(c) Theta Notation\n"
+q2 = "It defines the best case of an algorithm’s time complexity?\n(a) Big-O Notation\n(b) Omega Notation\n(c) Theta Notation\n"
+q3 = "It defines the average case of an algorithm’s time complexity?\n(a) Big-O Notation\n(b) Omega Notation\n(c) Theta Notation\n"
+
+questions = [
+  Question.new(q1, "a"),
+  Question.new(q2, "b"),
+  Question.new(q3, "c"),
+]
+
+def main(questions)
+  score = 0
+  puts "Welcome to the Quiz! Please type the letter corresponding to your answer."
+  questions.each do |question|
+    puts question.prompt
+    answer = ""
+    loop do
+      answer = gets.chomp().downcase
+      break if ['a', 'b', 'c'].include?(answer)
+      puts "Please enter a valid option (a, b, or c)."
+    end
+    score += 1 if answer == question.answer
+  end
+  puts "You got #{score}/#{questions.length} correct."
+end
+
+main(questions)
---
+++ 2 - Library - Add & Remove & list Books.rb
@@ -0,0 +1,43 @@
+class Library
+  def initialize
+    @books = []
+  end
+
+  def add_book(title)
+    @books << title
+    puts "'#{title}' has been added to the library."
+  end
+
+  def remove_book(title)
+    if @books.include?(title)
+      @books.delete(title)
+      puts "'#{title}' has been removed from the library."
+    else
+      puts "'#{title}' is not found in the library."
+    end
+  end
+
+  def list_books
+    if @books.empty?
+      puts "The library is empty."
+    else
+      puts "Books currently in the library:"
+      @books.each { |book| puts "- #{book}" }
+    end
+  end
+end
+
+# Example Usage:
+library = Library.new
+
+library.add_book("Great One") # Add One
+library.add_book("Hebta") # Add One
+library.add_book("Sera") # Add One
+
+library.list_books
+
+library.remove_book("Hebta")
+
+library.list_books
+
+library.remove_book("Ahmed's Story") # Not Exist
