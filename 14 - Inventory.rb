8d3758799bf79468e1513acf0ece5720980966db
AhmedKhaled146 <a7med74yaso@gmail.com>
2024-09-22 21:05:16 +0300

    setup project 14

---
+++ 14 - Inventory.rb
@@ -0,0 +1,6 @@
+# Write a Java program to create a class called "Inventory" with a collection of products and methods
+# to add and remove products, and to check for low inventory.
+class Inventory
+  attr_accessor :product_id, :product_name, :product_quantity
+
+end
